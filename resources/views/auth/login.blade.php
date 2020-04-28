a9fbf8971d735e516e1caf9b30131e8ddc9bba67
Anna <hakobyananna0105@gmail.com>
2020-04-28 20:46:39 +0400

    a

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,7 +60,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Авторизоваться
+                                        Войти
                                     </button>
 
                                     @if (Route::has('password.request'))
